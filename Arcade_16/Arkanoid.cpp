4e6fb890bd235ea69e18b30aa7043c6d6477f59a
Bor Brudar <bor.brudar@gmail.com>
2020-05-10 23:57:43 +0200

    bouncy ball

--- Arcade_16/Arkanoid.cpp
+++ Arcade_16/Arkanoid.cpp
@@ -7,6 +7,12 @@ Arkanoid::Arkanoid(Font& f)
 	text1.assign("Back");
 	back.setup(f, text1, Color::Blue, Vector2f(50, 30), Vector2f(580, 20), 14);
 
+	//setup ball
+	b.loadFromFile("res/arkanoid/ball.png");
+	ball.setTexture(b);
+	pos.x = 200; pos.y = 400;
+	ball.setPosition(pos);
+	ball.setScale(0.7, 0.7);
 	//setup block
 	for (int x = 0; x < sizeM; x++) {
 		for (int y = 0; y < sizeN; y++) {
@@ -15,16 +21,21 @@ Arkanoid::Arkanoid(Font& f)
 			blocks[x][y].setPosition(x * sizeX + offsetX + x * spacing, y * sizeY + offsetY + y * spacing);
 		}
 	}
-	
 }
 
 void Arkanoid::draw(RenderWindow& window)
 {
+	//ball
+	window.draw(ball);
+
+	//field of bricks
 	for (int x = 0; x < sizeM; x++) {
 		for (int y = 0; y < sizeN; y++) {
 			window.draw(blocks[x][y]);
 		}
 	}
+
+	//back button
 	back.draw(window);
 }
 
@@ -36,4 +47,14 @@ void Arkanoid::update(Mouse& mouse, RenderWindow& window, state& gameState, Even
 	}
 	if (back.isClicked(mouse, window)) gameState = state::menu;
 
+	//update ball position
+	pos.x += speedx;
+	pos.y += speedy;
+	//check for borders
+	if (pos.y >= 480) { pos.y = 480; speedy = -speedy; }
+	if (pos.y <= 0) { pos.y = 0; speedy = -speedy; }
+	if (pos.x <= 0) { pos.x = 0; speedx = -speedx; }
+	if (pos.x >= 480) { pos.x = 480; speedx = -speedx; }
+
+	ball.setPosition(pos);
 }
--- Arcade_16/Arkanoid.h
+++ Arcade_16/Arkanoid.h
@@ -1,9 +1,8 @@
 #pragma once
 #include "State.h"
-
+#include <iostream>
 
 constexpr int sizeM = 10, sizeN = 4;
-
 
 class Arkanoid : public State {
 public:
@@ -11,8 +10,13 @@ public:
 	void draw(RenderWindow& window);
 	void update(Mouse& mouse, RenderWindow& window, state& gameState, Event& e);
 
+	Texture b;
+	Sprite ball;
 	Button back;
 	RectangleShape blocks[sizeM][sizeN];
+	
 	int sizeX = 40, sizeY = 20;
 	int offsetX = 30, offsetY = 40, spacing = 2;
+	float speedx = 0.5, speedy = 0.5;
+	Vector2f pos;
 };
