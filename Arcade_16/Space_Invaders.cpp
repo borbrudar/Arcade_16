d73b13f66f4077d83ebc2aa96c092d4ebeebad71
Bor Brudar <bor.brudar@gmail.com>
2020-05-15 23:49:13 +0200

    invaders logitics

--- Arcade_16/Space_Invaders.cpp
+++ Arcade_16/Space_Invaders.cpp
@@ -2,14 +2,30 @@
 
 Space_Invaders::Space_Invaders(Font& f)
 {
+	//back button
 	std::string text1;
 	text1.assign("Back");
-	back.setup(f, text1, Color::Blue, Vector2f(50, 30), Vector2f(580, 20), 14);
+	back.setup(f, text1, Color::Blue, Vector2f(40, 25), Vector2f(580, 2), 12);
+
+	//invaders
+	invaders.resize(invdM);
+	for (int x = 0; x < invdM; x++) {
+		invaders[x].resize(invdN);
+		for (int y = 0; y < invdN; y++) {
+			invaders[x][y].setFillColor(Color::White);
+			invaders[x][y].setSize(Vector2f(sizeX, sizeY));
+			invaders[x][y].setPosition(x * (sizeX + spacing) + offsetX, y * (sizeY + spacing) + offsetY);
+		}
+	}
 }
 
 void Space_Invaders::draw(RenderWindow& window)
 {
 	back.draw(window);
+
+	for (int x = 0; x < invdM; x++) {
+		for (int y = 0; y < invdN; y++) window.draw(invaders[x][y]);
+	}
 }
 
 void Space_Invaders::update(Mouse& mouse, RenderWindow& window, state& gameState, Event& e)
--- Arcade_16/Space_Invaders.h
+++ Arcade_16/Space_Invaders.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "State.h"
+#include <vector>
+
+constexpr int invdM = 10, invdN = 5;
 
 class Space_Invaders : public State {
 public:
@@ -7,5 +10,9 @@ public:
 	void draw(RenderWindow& window);
 	void update(Mouse& mouse, RenderWindow& window, state& gameState, Event& e);
 
+	std::vector < std::vector<RectangleShape> > invaders;
 	Button back;
+
+	int offsetX = 30, offsetY = 40, spacing = 5;
+	int sizeX = 30, sizeY = 30;
 };
