1706b87aa3a9b09f72789aabe94a5bc18faefd5e
Bor Brudar <bor.brudar@gmail.com>
2020-05-26 19:29:53 +0200

    player added and line added, player movement

--- Arcade_16/Pong.cpp
+++ Arcade_16/Pong.cpp
@@ -5,20 +5,60 @@ Pong::Pong(Font& f)
 	//setup back button
 	std::string text1;
 	text1.assign("Back");
-	back.setup(f, text1, Color::Blue, Vector2f(50, 30), Vector2f(580, 30), 14);
+	back.setup(f, text1, Color::White, Vector2f(40, 30), Vector2f(scrWidth / 2 - 20, 10), 14);
+
+	//player setup
+	player.setFillColor(Color::White);
+	player.setSize(Vector2f(10, 40));
+	player.setPosition(50, scrHeight / 2);
+
+	//line
+	line.setFillColor(Color::White);
+	line.setSize(Vector2f(5, 20));
 }
 
 void Pong::draw(RenderWindow& window)
 {
+	window.clear(Color::Black);
+
+	//draw line
+	for (int i = 0; i < scrHeight / line.getSize().y / 2; i++) {
+		line.setPosition(scrWidth / 2, i * (line.getSize().y * 2) + line.getSize().y / 2);
+		window.draw(line);
+	}
 	back.draw(window);
+
+	window.draw(player);
+
+	
 }
 
 void Pong::update(Mouse& mouse, RenderWindow& window, state& gameState, Event& e)
 {
 	//clicky boi
-		while (window.pollEvent(e)) {
-			if (e.type == Event::Closed) window.close();
+	while (window.pollEvent(e)) {
+		if (e.type == Event::Closed) window.close();
+		if (e.type == Event::KeyPressed) {
+			if (e.key.code == Keyboard::Down) down = 1;
+			if (e.key.code == Keyboard::Up) up = 1;
 		}
+		if (e.type == Event::KeyReleased) {
+			if (e.key.code == Keyboard::Down) down = 0;
+			if (e.key.code == Keyboard::Up) up = 0;
+		}
+	}
 	if (back.isClicked(mouse, window)) gameState = state::menu;
 
+	if (up) speedy = -csped;
+	else if (down) speedy = csped;
+	else speedy = 0;
+
+	//border stuff
+	if (player.getPosition().y < 0) player.setPosition(player.getPosition().x, 0);
+	if (player.getPosition().y + player.getSize().y > scrHeight)
+	player.setPosition(player.getPosition().x, scrHeight - player.getSize().y);
+
+
+
+	player.move(0, speedy);
 }
--- Arcade_16/Pong.h
+++ Arcade_16/Pong.h
@@ -8,6 +8,11 @@ public:
 	Pong(Font& f);
 	void draw(RenderWindow& window);
 	void update(Mouse& mouse, RenderWindow& window, state& gameState, Event& e);
-
+	 
 	Button back;
+	RectangleShape line;
+	 
+	RectangleShape player;
+	bool up = 0, down = 0;
+	float speedy = 0, csped = 0.5f;
 };
